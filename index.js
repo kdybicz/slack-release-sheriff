6381be8b69923ee187ef940bfbef8ddd69bc9926
Kamil Dybicz <kamil@secretescapes.com>
2016-10-19 23:58:59 +0200

    dummy implementation of basic functionality

--- index.js
+++ index.js
@@ -1,77 +1,190 @@
-var Botkit = require('botkit')
+var Botkit = require('botkit');
 
-var token = process.env.SLACK_TOKEN
+var token = process.env.SLACK_TOKEN;
 
 var controller = Botkit.slackbot({
-  // reconnect to Slack RTM when connection goes bad
-  retry: Infinity,
-  debug: false
-})
+	json_file_store: 'notebook',
+	// reconnect to Slack RTM when connection goes bad
+	retry: Infinity,
+	debug: false
+});
 
 // Assume single team mode if we have a SLACK_TOKEN
 if (token) {
-  console.log('Starting in single-team mode')
-  controller.spawn({
-    token: token,
-    retry: Infinity
-  }).startRTM(function (err, bot, payload) {
-    if (err) {
-      throw new Error(err)
-    }
+	console.log('Starting in single-team mode')
+	controller.spawn({
+		token: token,
+		retry: Infinity
+	}).startRTM(function (err, bot, payload) {
+		if (err) {
+			throw new Error(err)
+		}
 
-    console.log('Connected to Slack RTM')
-  })
+		console.log('Connected to Slack RTM')
+	});
 // Otherwise assume multi-team mode - setup beep boop resourcer connection
 } else {
-  console.log('Starting in Beep Boop multi-team mode')
-  require('beepboop-botkit').start(controller, { debug: true })
+	console.log('Starting in Beep Boop multi-team mode');
+	require('beepboop-botkit').start(controller, {debug: true});
 }
 
 controller.on('bot_channel_join', function (bot, message) {
-  bot.reply(message, "I'm here!")
-})
+	bot.reply(message, "I'm here!");
+});
 
 controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
-  bot.reply(message, 'Hello.')
-})
+	bot.reply(message, 'Hello.')
+});
 
 controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
-  bot.reply(message, 'Hello.')
-  bot.reply(message, 'It\'s nice to talk to you directly.')
-})
+	bot.reply(message, 'Hello.');
+	bot.reply(message, 'It\'s nice to talk to you directly.');
+});
+
+controller.hears(['next'], ['direct_mention', 'direct_message'], function (bot, message) {
+
+	controller.storage.teams.get(message.team, function (error, team_data) {
+		if (team_data == undefined || team_data.release_queue.length == 0) {
+			bot.reply(message, 'The queue is empty!')
+		} else {
+			console.log(JSON.stringify(team_data.release_queue));
+			bot.reply(message, 'Next in the release queue is @' + team_data.release_queue[0])
+		}
+
+	})
+});
+
+controller.hears(['list'], ['direct_mention', 'direct_message'], function (bot, message) {
+
+	controller.storage.teams.get(message.team, function (error, team_data) {
+
+		if (team_data == undefined || team_data.release_queue.length == 0) {
+			bot.reply(message, 'The release queue is empty!')
+		} else {
+			var list = team_data.release_queue.reduce(function (total, currentValue) {
+				return total + '@' + currentValue + '\n';
+			}, '');
+			bot.reply(message, 'Current release queue in order:\n' + list)
+		}
+	})
+});
+
+controller.hears(['add'], ['direct_mention', 'direct_message'], function (bot, message) {
+
+	bot.api.users.info({user: message.user}, function (error, response) {
+
+		controller.storage.teams.get(message.team, function (error, team_data) {
+
+			if (team_data == undefined) {
+				team_data = {id: message.team, release_queue: []}
+			}
+
+			if (team_data.release_queue.indexOf(response.user.name) != -1) {
+				bot.reply(message, '@' + response.user.name + ' you are already in the release queue')
+			} else {
+				team_data.release_queue.push(response.user.name);
+
+				bot.reply(message, 'I\'m adding you @' + response.user.name + ' to the release queue')
+			}
+
+			controller.storage.teams.save(team_data, function (error) {
+				if (error != undefined) {
+					console.error(error)
+				}
+			})
+		})
+	})
+});
+
+controller.hears(['remove'], ['direct_mention', 'direct_message'], function (bot, message) {
+
+	bot.api.users.info({user: message.user}, function (error, response) {
+
+		controller.storage.teams.get(message.team, function (error, team_data) {
+			if (team_data == undefined) {
+				team_data = {id: message.team, release_queue: []}
+			}
+
+			var index = team_data.release_queue.indexOf(response.user.name);
+			if (index < 0) {
+				bot.reply(message, '@' + response.user.name + ' you are not in the release queue')
+			} else {
+				team_data.release_queue.splice(index, 1);
+
+				bot.reply(message, 'I\'m removing you @' + response.user.name + ' from the release queue')
+			}
+
+			controller.storage.teams.save(team_data, function (error) {
+				if (error != undefined) {
+					console.error(error)
+				}
+			})
+		})
+	});
+});
+
+controller.hears(['skip'], ['direct_mention', 'direct_message'], function (bot, message) {
+
+	bot.api.users.info({user: message.user}, function (error, response) {
+
+		controller.storage.teams.get(message.team, function (error, team_data) {
+
+			if (team_data == undefined) {
+				team_data = {id: message.team, release_queue: []}
+			}
+
+			if (team_data.release_queue.length == 0) {
+				bot.reply(message, 'The release queue is empty!')
+			} else {
+				var index = team_data.release_queue.indexOf(response.user.name);
+				if (index < 0) {
+					bot.reply(message, '@' + response.user.name + ' you are not in the release queue')
+				} else if (index == 0 && team_data.release_queue.length == 1) {
+					bot.reply(message, '@' + response.user.name + ' you are the only one in the release queue')
+				} else {
+					team_data.release_queue.splice(index, 1);
+					team_data.release_queue.push(response.user.name);
+
+					bot.reply(message, 'I\'m moving you @' + response.user.name + ' at the end of the release queue')
+				}
+			}
+
+			controller.storage.teams.save(team_data, function (error) {
+				if (error != undefined) {
+					console.error(error)
+				}
+			})
+		})
+	})
+});
+
+controller.hears(['cleanup'], ['direct_mention', 'direct_message'], function (bot, message) {
+
+	bot.reply(message, 'I\'m removing all people from the release queue')
+});
+
+controller.hears('A deployment has just been started!', ['message_received'], function (bot, message) {
+	bot.api.users.info({user: message.user}, function (error, response) {
+		bot.reply(message, 'Next in the release queue is: @' + response.user.name)
+	})
+});
 
 controller.hears('.*', ['mention'], function (bot, message) {
-  bot.reply(message, 'You really do care about me. :heart:')
-})
+	bot.reply(message, 'Sorry, I don\'t get that. Can you be more specific?')
+});
 
 controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
-  var help = 'I will respond to the following messages: \n' +
-      '`bot hi` for a simple message.\n' +
-      '`bot attachment` to see a Slack attachment message.\n' +
-      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
-      '`bot help` to see this again.'
-  bot.reply(message, help)
-})
-
-controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
-  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
-  var attachments = [{
-    fallback: text,
-    pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
-    title: 'Host, deploy and share your bot in seconds.',
-    image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-    title_link: 'https://beepboophq.com/',
-    text: text,
-    color: '#7CD197'
-  }]
-
-  bot.reply(message, {
-    attachments: attachments
-  }, function (err, resp) {
-    console.log(err, resp)
-  })
-})
+	var help = 'I will respond to the following messages: \n' +
+		'`@' + bot.identity.name + ' hi` for a simple message.\n' +
+		'`@' + bot.identity.name + ' add` to add yourself at the end of release queue.\n' +
+		'`@' + bot.identity.name + ' remove` to remove yourself from the release queue.\n' +
+		'`@' + bot.identity.name + ' cleanup` to remove all from the release queue.\n' +
+		'`@' + bot.identity.name + ' next` for next in the release queue.\n' +
+		'`@' + bot.identity.name + ' list` shows whole release queue.\n' +
+		'`@' + bot.identity.name + ' help` to see this again.'
+	bot.reply(message, help)
+});
 
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
-})
+	bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
+});
