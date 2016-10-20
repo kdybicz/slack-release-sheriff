5c8d19e533310b7f75dc02eb9fd829a173a4a22a
Kamil Dybicz <kamil@secretescapes.com>
2016-10-20 09:44:45 +0200

    adding bit more meat

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Botkit = require('botkit');
+var os = require('os');
 
 var token = process.env.SLACK_TOKEN;
 
@@ -45,10 +46,10 @@ controller.hears(['next'], ['direct_mention', 'direct_message'], function (bot, 
 
 	controller.storage.teams.get(message.team, function (error, team_data) {
 		if (team_data == undefined || team_data.release_queue.length == 0) {
-			bot.reply(message, 'The queue is empty!')
+			bot.reply(message, 'The release queue is empty!')
 		} else {
 			console.log(JSON.stringify(team_data.release_queue));
-			bot.reply(message, 'Next in the release queue is @' + team_data.release_queue[0])
+			bot.reply(message, 'Next in the release queue is <@' + team_data.release_queue[0] + '>')
 		}
 
 	})
@@ -62,7 +63,7 @@ controller.hears(['list'], ['direct_mention', 'direct_message'], function (bot, 
 			bot.reply(message, 'The release queue is empty!')
 		} else {
 			var list = team_data.release_queue.reduce(function (total, currentValue) {
-				return total + '@' + currentValue + '\n';
+				return total + '<@' + currentValue + '>\n';
 			}, '');
 			bot.reply(message, 'Current release queue in order:\n' + list)
 		}
@@ -80,11 +81,11 @@ controller.hears(['add'], ['direct_mention', 'direct_message'], function (bot, m
 			}
 
 			if (team_data.release_queue.indexOf(response.user.name) != -1) {
-				bot.reply(message, '@' + response.user.name + ' you are already in the release queue')
+				bot.reply(message, '<@' + response.user.name + '> you are already in the release queue')
 			} else {
 				team_data.release_queue.push(response.user.name);
 
-				bot.reply(message, 'I\'m adding you @' + response.user.name + ' to the release queue')
+				bot.reply(message, 'I\'m adding you <@' + response.user.name + '> to the release queue')
 			}
 
 			controller.storage.teams.save(team_data, function (error) {
@@ -107,11 +108,11 @@ controller.hears(['remove'], ['direct_mention', 'direct_message'], function (bot
 
 			var index = team_data.release_queue.indexOf(response.user.name);
 			if (index < 0) {
-				bot.reply(message, '@' + response.user.name + ' you are not in the release queue')
+				bot.reply(message, '<@' + response.user.name + '> you are not in the release queue')
 			} else {
 				team_data.release_queue.splice(index, 1);
 
-				bot.reply(message, 'I\'m removing you @' + response.user.name + ' from the release queue')
+				bot.reply(message, 'I\'m removing you <@' + response.user.name + '> from the release queue')
 			}
 
 			controller.storage.teams.save(team_data, function (error) {
@@ -120,6 +121,41 @@ controller.hears(['remove'], ['direct_mention', 'direct_message'], function (bot
 				}
 			})
 		})
+	});
+});
+
+controller.hears(['cleanup'], ['direct_mention', 'direct_message'], function (bot, message) {
+
+	bot.api.users.info({user: message.user}, function (error, response) {
+
+		bot.startConversation(message, function (err, convo) {
+
+			convo.ask('Are you sure you want me to remove everyone from release queue?', [
+				{
+					pattern: bot.utterances.yes,
+					callback: function (response, convo) {
+
+						var team_data = {id: message.team, release_queue: []};
+						controller.storage.teams.save(team_data, function (error) {
+							if (error != undefined) {
+								console.error(error)
+							}
+						});
+
+						convo.say('Done!');
+						convo.next();
+					}
+				},
+				{
+					pattern: bot.utterances.no,
+					default: true,
+					callback: function (response, convo) {
+						convo.say('*Phew!*');
+						convo.next();
+					}
+				}
+			]);
+		});
 	});
 });
 
@@ -138,14 +174,14 @@ controller.hears(['skip'], ['direct_mention', 'direct_message'], function (bot, 
 			} else {
 				var index = team_data.release_queue.indexOf(response.user.name);
 				if (index < 0) {
-					bot.reply(message, '@' + response.user.name + ' you are not in the release queue')
+					bot.reply(message, '<@' + response.user.name + '> you are not in the release queue')
 				} else if (index == 0 && team_data.release_queue.length == 1) {
-					bot.reply(message, '@' + response.user.name + ' you are the only one in the release queue')
+					bot.reply(message, '<@' + response.user.name + '> you are the only one in the release queue')
 				} else {
 					team_data.release_queue.splice(index, 1);
 					team_data.release_queue.push(response.user.name);
 
-					bot.reply(message, 'I\'m moving you @' + response.user.name + ' at the end of the release queue')
+					bot.reply(message, 'I\'m moving you <@' + response.user.name + '> at the end of the release queue')
 				}
 			}
 
@@ -165,13 +201,39 @@ controller.hears(['cleanup'], ['direct_mention', 'direct_message'], function (bo
 
 controller.hears('A deployment has just been started!', ['message_received'], function (bot, message) {
 	bot.api.users.info({user: message.user}, function (error, response) {
-		bot.reply(message, 'Next in the release queue is: @' + response.user.name)
+		bot.reply(message, 'Next in the release queue is: <@' + response.user.name + '>')
 	})
 });
 
 controller.hears('.*', ['mention'], function (bot, message) {
 	bot.reply(message, 'Sorry, I don\'t get that. Can you be more specific?')
 });
+
+controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'], 'direct_message,direct_mention,mention', function (bot, message) {
+
+	var hostname = os.hostname();
+	var uptime = formatUptime(process.uptime());
+
+	bot.reply(message, ':robot_face: I am a bot named <@' + bot.identity.name + '>. I have been running for ' + uptime + ' on ' + hostname + '.');
+});
+
+function formatUptime(uptime) {
+	var unit = 'second';
+	if (uptime > 60) {
+		uptime = uptime / 60;
+		unit = 'minute';
+	}
+	if (uptime > 60) {
+		uptime = uptime / 60;
+		unit = 'hour';
+	}
+	if (uptime != 1) {
+		unit = unit + 's';
+	}
+
+	uptime = uptime + ' ' + unit;
+	return uptime;
+}
 
 controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
 	var help = 'I will respond to the following messages: \n' +
